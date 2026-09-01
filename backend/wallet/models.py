13d4c505d3518a3f2660b24717c7c3c1fdeedb3f
Tinto57 <nathan.maillard@epitech.eu>
2026-09-01 11:18:35 +0200

    [ADD] wallet app with ready model

---
+++ backend/wallet/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Employee(models.Model):
+    user = models.OneToOneField('accounts.User', on_delete=models.CASCADE)
+    employer = models.CharField(max_length=200)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"{self.user.username}: ({self.balance}€)"
+
+class TopUp(models.Model):
+    user = models.OneToOneField(Employee, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey('accounts.User', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"+{self.amount}€ for {self.employee.user.username}"
