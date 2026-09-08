e4a4bf407219b70bccdaf3a371a37ae196bb22dc
Tinto57 <nathan.maillard@epitech.eu>
2026-09-08 16:37:32 +0200

    [DEL] Remove commented-out MinisterSpotlight routes

--- backend/partners/urls.py
+++ backend/partners/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
 from .views import (
     CategoriesView,
-    # MinisterSpotlightClickView,
-    # MinisterSpotlightListCreateView,
-    # MinisterSpotlightPublicListView,
-    # MinisterSpotlightPublicView,
-    # MinisterSpotlightRepublishView,
     PartnersView,
     PartnerMeView,
     SinglePartnerView,
@@ -20,9 +15,4 @@ urlpatterns = [
     path('partners/<int:partner_id>/', SinglePartnerView.as_view(), name='partner-detail'),
     path('partners/<int:partner_id>/decision/', PartnerDecisionCreateView.as_view(), name='partner-decision-create'),
     path('partners/<int:partner_id>/decisions/', PartnerDecisionsListView.as_view(), name='partner-decisions-list'),
-    # path('ministre/coup-de-coeur/', MinisterSpotlightPublicView.as_view(), name='minister-spotlight-public'),
-    # path('ministre/coup-de-coeur/toutes/', MinisterSpotlightPublicListView.as_view(), name='minister-spotlight-public-list'),
-    # path('ministre/coup-de-coeur/click/', MinisterSpotlightClickView.as_view(), name='minister-spotlight-click'),
-    # path('ministre/coup-de-coeur/historique/', MinisterSpotlightListCreateView.as_view(), name='minister-spotlight-history'),
-    # path('ministre/coup-de-coeur/<int:spotlight_id>/republier/', MinisterSpotlightRepublishView.as_view(), name='minister-spotlight-republish'),
 ]
